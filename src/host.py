9be223e3dd9096ed85094dd7424a3703cca5514f
bocai <whenbber@gmail.com>
2017-04-27 15:08:44 +0800

    增加消息过滤

--- src/host.py
+++ src/host.py
@@ -1,4 +1,4 @@
-#-*- encoding: UTF-8 -*-
+# -*- encoding: UTF-8 -*-
 
 import logging
 import time
@@ -6,13 +6,15 @@ import login_service
 import netstream
 import service_dispatcher
 
+
 class MainService(object):
     def __init__(self):
-        self.host = netstream.nethost()
+        self.host = netstream.nethost(8)
         self.host.startup(7890, '127.0.0.1')
         self.shutdown = False
         self.dispatcher = service_dispatcher.ServiceDispather(self.host)
         self.__setupServices()
+        self.clientLastMsgMap = {}
         self.__startLoop()
 
     def __setupServices(self):
@@ -24,9 +26,14 @@ class MainService(object):
             self.host.process()
             if len(self.host.queue) > 0:
                 logging.debug(self.host.queue)
-            event, hid, tag, data = self.host.read()
+            msg = self.host.read()
+            event, hid, tag, data = msg
+            if self.clientLastMsgMap.has_key(str(hid)) \
+                    and msg == self.clientLastMsgMap[str(hid)][0] \
+                    and time.time() - self.clientLastMsgMap[str(hid)][1] < 1:
+                continue
+            self.clientLastMsgMap[str(hid)] = (msg, time.time())
             if event != -1:
-                print event
                 if event == netstream.NET_NEW:
                     self.__handleNew(hid)
                 elif event == netstream.NET_LEAVE:
@@ -36,13 +43,8 @@ class MainService(object):
                 elif event == netstream.NET_TIMER:
                     pass
 
-    def __handleNew(self, wparam):
-        # self.host.settag(wparam, wparam)
-        self.host.nodelay(wparam, 1)
+    def __handleNew(self, hid):
+        self.host.nodelay(hid, 1)
 
     def __handleData(self, data, hid):
-        print data
         self.dispatcher.dispatch(data, hid)
-
-
-
